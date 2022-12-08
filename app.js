6610f14ae8561a06a0f5ea6093db7e04ed535ae0
Adam Office PC <teccmate@gmail.com>
2022-12-08 18:13:09 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//  this is a js file
+
+
+console.log("this is JS")
+
+
+function promfunc(){
+
+    const prom = new Promise((res) => {
+        res("This is resolveee prom")
+    });
+    return prom;
+}
+    
+async function callfunc(){
+    const result = await promfunc();
+    console.log(result)
+}
+
+
+callfunc();
